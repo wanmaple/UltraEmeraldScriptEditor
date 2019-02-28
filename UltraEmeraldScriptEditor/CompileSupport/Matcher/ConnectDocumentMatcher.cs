845dcabde7058130a997b969c1b3b858521fcdc4
Winder <wanxin@lilith.sh>
2019-02-28 21:07:46 +0800

    matcher

--- UltraEmeraldScriptEditor/CompileSupport/Matcher/ConnectDocumentMatcher.cs
+++ UltraEmeraldScriptEditor/CompileSupport/Matcher/ConnectDocumentMatcher.cs
@@ -9,17 +9,20 @@ namespace CompileSupport.Matcher
 {
     public sealed class ConnectDocumentMatcher : IDocumentMatcher
     {
-        public List<Regex> FullyMatchRegexList => _fullyMatchRegexList;
+        public List<Regex> PrimaryRegexList => _primaryRegexList;
+        public List<Regex> SecondaryRegexList => _secondaryRegexList;
         public List<Regex> ConnectRegexList => _connectRegexList;
 
         #region ConnectDocumentMatcher
         public ConnectDocumentMatcher(ITextSource source)
         {
             _source = source ?? throw new ArgumentNullException("source");
+            _content = _source.Text;
             _currentOffset = 0;
-            _fullyMatchRegexList = new List<Regex>();
+            _primaryRegexList = new List<Regex>();
             _connectRegexList = new List<Regex>();
-        } 
+            _nextSegment = null;
+        }
         #endregion
 
         #region IDocumentMatcher
@@ -31,7 +34,7 @@ namespace CompileSupport.Matcher
         {
             if (startOffset < 0 || startOffset > _source.Length)
             {
-                return null;
+                throw new ArgumentOutOfRangeException("startOffset");
             }
             _currentOffset = startOffset;
             return MatchNext();
@@ -39,14 +42,44 @@ namespace CompileSupport.Matcher
 
         public ISegment MatchNext()
         {
-            throw new NotImplementedException();
+            Int32 matchOffset = _currentOffset;
+            // 先过滤空格和换行符
+            while (_content[matchOffset] == ' ' || _content[matchOffset] == '\r' || _content[matchOffset] == '\n')
+            {
+                ++matchOffset;
+            }
+            Match match = null;
+            ISegment ret = null;
+            foreach (var regex in _primaryRegexList)
+            {
+                match = regex.Match(_content, matchOffset);
+                if (match.Length > 0 && match.Index == matchOffset)
+                {
+                    ret = new SimpleSegment(match.Index, match.Length);
+                    matchOffset += match.Length;
+                    return ret;
+                }
+            }
+            foreach (var regex in _secondaryRegexList)
+            {
+                match = regex.Match(_content, matchOffset);
+                if (match.Length > 0 && match.Index == matchOffset)
+                {
+                    ret = new SimpleSegment(match.Index, match.Length);
+                    matchOffset += match.Length;
+                    return ret;
+                }
+            }
+            return ret;
         }
         #endregion
 
         private ITextSource _source;
+        private String _content;
         private Int32 _currentOffset;
-        private ISegment _nextSegement;
-        private List<Regex> _fullyMatchRegexList;
+        private ISegment _nextSegment;
+        private List<Regex> _primaryRegexList;
+        private List<Regex> _secondaryRegexList;
         private List<Regex> _connectRegexList;
     }
 }
