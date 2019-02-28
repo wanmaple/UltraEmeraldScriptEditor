ce5e0a75c64f0345f65b44e97b2a088884c78369
Winder <wanxin@lilith.sh>
2019-02-28 19:46:07 +0800

    matcher

---
+++ UltraEmeraldScriptEditor/CompileSupport/Matcher/ConnectDocumentMatcher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using EditorSupport.Document;
+
+namespace CompileSupport.Matcher
+{
+    public sealed class ConnectDocumentMatcher : IDocumentMatcher
+    {
+        public List<Regex> FullyMatchRegexList => _fullyMatchRegexList;
+        public List<Regex> ConnectRegexList => _connectRegexList;
+
+        #region ConnectDocumentMatcher
+        public ConnectDocumentMatcher(ITextSource source)
+        {
+            _source = source ?? throw new ArgumentNullException("source");
+            _currentOffset = 0;
+            _fullyMatchRegexList = new List<Regex>();
+            _connectRegexList = new List<Regex>();
+        } 
+        #endregion
+
+        #region IDocumentMatcher
+        public ITextSource Document => _source;
+
+        public int CurrentOffset { get => _currentOffset; set => _currentOffset = value; }
+
+        public ISegment MatchFrom(int startOffset)
+        {
+            if (startOffset < 0 || startOffset > _source.Length)
+            {
+                return null;
+            }
+            _currentOffset = startOffset;
+            return MatchNext();
+        }
+
+        public ISegment MatchNext()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        private ITextSource _source;
+        private Int32 _currentOffset;
+        private ISegment _nextSegement;
+        private List<Regex> _fullyMatchRegexList;
+        private List<Regex> _connectRegexList;
+    }
+}
---
+++ UltraEmeraldScriptEditor/CompileSupport/Matcher/IDocumentMatcher.cs
@@ -0,0 +1,35 @@
+﻿using EditorSupport.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompileSupport.Matcher
+{
+    /// <summary>
+    /// 文本匹配器。
+    /// </summary>
+    public interface IDocumentMatcher
+    {
+        /// <summary>
+        /// 目标文档
+        /// </summary>
+        ITextSource Document { get; }
+        /// <summary>
+        /// 当前偏移
+        /// </summary>
+        Int32 CurrentOffset { get; set; }
+
+        /// <summary>
+        /// 从指定偏移处匹配
+        /// </summary>
+        /// <param name="startOffset"></param>
+        /// <returns></returns>
+        ISegment MatchFrom(Int32 startOffset);
+        /// <summary>
+        /// 匹配下一处
+        /// </summary>
+        /// <returns></returns>
+        ISegment MatchNext();
+    }
+}
