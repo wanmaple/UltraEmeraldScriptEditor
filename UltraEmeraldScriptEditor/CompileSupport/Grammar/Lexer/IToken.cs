91651cc71e66912fc655efcb5dd8ce426f4b0140
Winder <wanxin@lilith.sh>
2019-03-01 20:13:46 +0800

    grammar

---
+++ UltraEmeraldScriptEditor/CompileSupport/Grammar/Lexer/IToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompileSupport.Grammar.Lexer
+{
+    public interface IToken
+    {
+        Int32 Tag { get; }
+    }
+}
---
+++ UltraEmeraldScriptEditor/CompileSupport/Grammar/Lexer/Token.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompileSupport.Grammar.Lexer
+{
+    public class Token : IToken
+    {
+        public int Tag => _tag;
+
+        public Token(Int32 tag)
+        {
+            _tag = tag;
+        }
+
+        protected Int32 _tag;
+    }
+}
